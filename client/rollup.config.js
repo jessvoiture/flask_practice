2dba06e450b33d5b176b33a9a11aa0edc310b01d
jessvoiture <jessicascarr989@gmail.com>
2023-06-13 14:29:14 +0100

    removed terser:

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -2,7 +2,6 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -38,11 +37,10 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload('public')
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
 	],
 	watch: {
 		clearScreen: false
